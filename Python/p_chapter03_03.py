033a7cf8cb52e6d620336143a40ecc3ae5cbc5e0
sangwoo <psu8430@gmail.com>
2020-09-01 03:29:55 +0900

    Update Study Data 8/31

---
+++ Python/p_chapter03_03.py
@@ -0,0 +1,107 @@
+# Chapter03-03
+# Special Method(Magic Method)
+# 파이썬의 핵심 -> 시퀀스(Sequence), 반복(Iterator), 함수(Function), 클래스(Class)
+# 클래스 안에 정의할 수 있는 특별한(Built in) 메소드
+
+# 객체 -> 파이썬의 데이터를 추상화
+# 모든 객체 -> id, type -> value
+
+# 일반적인 튜플 값의 변경이 불가능 하다.
+pt1 = (1.0, 5.0)
+pt2 = (2.5, 1.5)
+
+from math import sqrt
+
+l_leng1 = sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
+
+print(l_leng1)
+
+# 네임드 튜플 사용
+from collections import namedtuple
+
+# 네임드 튜플 선언
+Point = namedtuple('Point', 'x y')
+
+pt3 = Point(1.0, 5.0)
+pt4 = Point(2.5, 1.5)
+
+
+l_leng2 = sqrt((pt3.x - pt4.x) ** 2 + (pt3.y - pt4.y) ** 2)
+print(l_leng2)
+
+
+# 네임드 튜플 선언 방법
+Point1 = namedtuple('Point', ['x', 'y'])
+Point2 = namedtuple('Point', 'x, y')
+Point3 = namedtuple('Point', 'x y')
+Point4 = namedtuple('Point', 'x y x class', rename=True) # class 와 같이 예약어인 경우 변수 명으로 사용할 때 rename을 사용하여 재설정 한다. Default = False
+
+# 출력
+print(Point1, Point2, Point3, Point4)
+
+# Dict to unpacking
+temp_dict = {'x': 75, 'y': 55}
+
+# 객체 생성
+p1 = Point1(x=12, y=321)
+p2 = Point2(20, 40)
+p3 = Point3(20 , y = 23)
+p4 = Point4(10, 20, 30, 40) # 결과 : Point(x=10, y=20, _2=30, _3=40) -> 중복 되는 key 값이나 예약어는 알아서 난수를 생성하여 값을 할당.
+p5 = Point3(**temp_dict) # dict 자료형을 unpacking하여 값을 할당
+
+print()
+print(p1)
+print(p2)
+print(p3)
+# rename 테스트
+print(p4)
+print(p5)
+
+
+# 사용
+print(p1[0] + p2[1]) # 인덱스로 접근
+print(p1.x + p2.y) # 키로 접근
+
+x, y = p3 # unpacking
+print(x, y)
+
+# 네임트 튜플 메소드
+temp = [52, 38]
+
+#._make() : 새로운 객체를 생성
+p4 = Point1._make(temp) # list 를 namedTuple로 만들어 주는 method -> _make, 변수의 개수를 맞춰 주어야 한다.
+
+print(p4)
+
+# _fields : 필드 네임 확인
+print(p1._fields, p2._fields, p3._fields)
+
+# _asdict() : OrderedDict 반환
+print(p1._asdict()) # result => OrderedDict([('x', 12), ('y', 321)])
+print(p4._asdict())
+
+# 실 사용 실습
+# 반 20명, 4개의 반(A, B, C, D)
+
+Classes = namedtuple("Classes", ['rank', 'number'])
+
+# 그룹 리스트 선언
+numbers = [str(n) for n in range(1, 21)]
+ranks = 'A B C D'.split()
+
+
+# List Comprehension
+students = [Classes(rank, number) for rank in ranks for number in numbers] # 총 80 명의 학생 객체 생성
+
+# 추천
+students2 = [Classes(rank, number)
+                for rank in 'A B C D'.split()
+                for number in [str(n)
+                    for n in range(1, 21)]]
+
+print(len(students))
+print(len(students2))
+
+# 출력
+for s in students2 : 
+    print(s)
---
+++ Python/p_chapter04_01.py
@@ -0,0 +1,83 @@
+# Chapter04-10
+# 시퀀스 형
+# 컨테이너(Container : 서로다른 자료형[list, tuple, set, dictionary, collections.deque])
+# ex) a = [3, 3.0, 'A']
+# 플랫 (Flat : 한 개의 자료형[str, bytes, bytearray, array.array, memoryview])
+# 가변( list, bytearray, array.array, memoryview, deque )
+# 불변( tuple, str, bytes )
+
+# 리스트 및 튜플 고급
+
+# 지능형 리스트 (Comprehending lists)
+chars = '+_)(*&^%$#@!)'
+
+# char[2] = a -> str 상태의 데이터는 변경이 불가능하다.
+code_list1 = []
+
+for s in chars :
+    #유니코드 리스트
+    code_list1.append(ord(s)) # ord() -> 문자를 아스키 코드로 변환
+
+print(code_list1)
+
+# Comprehending lists
+code_list2 = [ord(s) for s in chars]
+
+print(code_list2)
+
+# Comprehending Lists + Map, Filter
+code_list3 = [ord(s) for s in chars if ord(s) > 40]
+print(code_list3)
+
+code_list4 = list(filter(lambda x : x > 40, map(ord, chars)))
+#filter((익명)함수, 처리할 데이터)
+print(code_list4)
+
+
+print()
+print('전체 출력(유니코드)')
+print(code_list1)
+print(code_list2)
+print(code_list3)
+print(code_list4)
+print('전체 출력(문자)')
+print([chr(s) for s in code_list1])
+print([chr(s) for s in code_list2])
+print([chr(s) for s in code_list3])
+print([chr(s) for s in code_list4])
+
+# Generator 생성 --> 연속되는 값을 만들어 낸다.
+import array 
+
+# Generator : 한 번에 한 개의 항목을 생성( 메모리 유지 X )
+tuple_g = (ord(s) for s in chars)
+array_g = array.array('I', (ord(s) for s in chars)) # --> 'I' == int
+print(type(tuple_g))
+print(next(tuple_g)) # --> next() 로 하나 씩 출력
+print(type(array_g))
+print(array_g)
+print(array_g.tolist()) # --> array 상태를 리스트로 바꾸어 준다.
+
+# Generator 예제
+print(('%s' % c + str(n) for c in ['A', 'B', 'C', 'D'] for n in range(1, 21)))
+
+for s in ('%s' % c + str(n) for c in ['A', 'B', 'C', 'D'] for n in range(1, 21)):
+    print(s) # --> 하나하나 만들면서 하나하나 출력
+
+# 리스트 주의 (deep copy, shallow copy)
+marks1 = [['~'] * 3 for _ in range(4)] # 반복문 사용시 사용하지 않는 변수는 '_'로도 사용 가능하다.
+marks2 = [['~'] * 3] * 4
+
+print(marks1)
+print(marks2)
+print()
+# 수정
+marks1[0][1] = 'X' # 
+marks2[0][1] = 'X' # 의도하지 않은 값의 변경이 발생한다 하나의 주소값이 여러번 복사되어 전체 데이터에 영향을 준다
+
+print(marks1)
+print(marks2)
+
+# 확인
+print([id(i) for i in marks1])
+print([id(i) for i in marks2])
